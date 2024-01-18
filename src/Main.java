60c3ecdaea63669d67a9f4c32464885cd15a0af1
harukasato0215 <popo02150125@gmail.com>
2024-01-18 21:07:08 +0900

    0118

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -28,23 +29,38 @@ public class Main {
 			t3.name ="伝説の宝石";
 			t3.rare = 3;
 			t3.price = 10000;
-
+		
 			Treasure[] types = {t1,t2,t3};
 
+			
+			//財布とその中身
+			Wallet main;
+			main =new Wallet();
+			main.name = "財布";
+			main.money = 0;
+			
+			
+			
 		//選択画面
 		while(true) {
 			System.out.print("1:探検に出る　2:宝を売る　3:終了");
-			int selct =new Scanner(System.in).nextInt();
+			int select =new Scanner(System.in).nextInt();
 
-			switch(selct) {
+			switch(select) {
 			case 1://探検にでる
 				//見つけられる宝を作成
 					int rand = new Random().nextInt(types.length);
-					String treasure = types[rand].name;
-					System.out.println(treasure + "を見つけた！");
+					String treasureName = types[rand].name;
+					int treasureRare = types[rand].rare;
+					int treasurePrice = types[rand].price;
+					System.out.println(treasureName + "を見つけた！レア度：" + treasureRare);
 				//見つけた宝物を所持する
-					Treasure treasere =new Treasure();
-					treasures.add(treasere);
+					
+					Treasure treasure =new Treasure();
+					treasure.name =treasureName;
+					treasure.rare = treasureRare;
+					treasure.price = treasurePrice;
+					treasures.add(treasure);
 					break;
 
 			case 2://宝を売る
@@ -52,14 +68,17 @@ public class Main {
 					System.out.println("まだ宝物を見つけてないよ！");
 				}else {//宝物を持っているとき
 					while(true) {
-						System.out.println("どの宝物を売る？");
-						for(Treasure t:treasures ) {//持っている宝物を表示
-							System.out.printf("%s[%s](%d)\n", t.name, t.rare, t.price);
+						for (int i = 0; i < treasures.size(); i++) {
+							System.out.printf("%d・・・%s[%s](%d)\n", i, treasures.get(i).name, treasures.get(i).rare, treasures.get(i).price);
 						}
 						//どの宝物にするか選択
+						System.out.println("どの宝物を売る？");
+						select = new Scanner(System.in).nextInt();
+						//売れたものを表示 財布に売ったモノの値段をいれる
+						treasures.remove(select);
+						
 						//”～～円で売れた！　所持金　～～円と出力
-
-
+						System.out.println(treasures.get(select).price +"円で売れた！所持金" + main.money);
 						System.out.println("ほかのものも売る？");//続けるかどうかの確認
 						System.out.println("1:はい　2:いいえ");
 						int reply =new Scanner(System.in).nextInt();
@@ -72,13 +91,21 @@ public class Main {
 				break;
 			case 3:
 				System.out.println("!!!結果発表!!!");
-				break;
+				//お宝の表示
+				System.out.print("～見つけた宝物～");
+				for(Treasure t:treasures ) {//持っている宝物を表示
+					System.out.printf("%s[%s](%d)\n", t.name, t.rare, t.price);
+				}
+				//お金の表示
+				System.out.print("～持っているお金～");
+				System.out.println(main.money + "円");
+				System.out.println("また遊んでね(^▽^)/");
+				return;
 			default:
-				System.out.println("1～3を選んでね");
-				break;
+				System.out.println("1～3で選んでね！");
 			}
 		}
 
-	}
+}
 
 }
